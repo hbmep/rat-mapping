3533f89abb8f045ea582ea7f0e21b1ad26f5fd4b
Andres Pascual-Leone <andres@andress-mbp.dyn.nyp.org>
2024-05-13 12:56:14 -0400

    updated paths

--- notebooks/basic-setup/constants.py
+++ notebooks/basic-setup/constants.py
@@ -1,8 +1,8 @@
 import os
 
-TOML_PATH = "/home/vishu/repos/rat-mapping-paper/configs/basic-setup/config.toml"
-DATA_PATH = "/home/vishu/repos/rat-mapping-paper/notebooks/basic-setup/sample_data.csv"
-# BUILD_DIR = os.path.join(PAPER_DIR, "reports", "basic-setup")
+TOML_PATH = "/Users/andres/repos/rat-mapping-paper/configs/basic-setup/config.toml"
+DATA_PATH = "/Users/andres/repos/rat-mapping-paper/notebooks/basic-setup/sample_data.csv"
+BUILD_DIR = "/Users/andres/repos/rat-mapping-paper/reports/basic-setup/"
 
 INFERENCE_FILE = "inference.pkl"
 NETCODE_FILE = "inference_data.nc"
