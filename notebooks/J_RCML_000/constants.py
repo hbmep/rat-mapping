00e76e5369d909c2a22daca632825389063ef33f
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-05-13 15:54:23 -0400

    add non-hierarchical setup

---
+++ notebooks/J_RCML_000/constants.py
@@ -0,0 +1,6 @@
+TOML_PATH = "/home/vishu/repos/rat-mapping-paper/configs/J_RCML_000/config.toml"
+DATA_PATH = "/home/vishu/data/hbmep-processed/J_RCML_000/data.csv"
+
+INFERENCE_FILE = "inference.pkl"
+
+BUILD_DIR = "/home/vishu/repos/rat-mapping-paper/reports/J_RCML_000"
