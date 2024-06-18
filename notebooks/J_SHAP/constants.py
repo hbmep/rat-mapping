1961f16870f7ce682dfc2f09111fe42ef4f47151
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-06-18 12:26:05 -0400

    add script to run nhb model on J_SHAP data

---
+++ notebooks/J_SHAP/constants.py
@@ -0,0 +1,6 @@
+TOML_PATH = "/home/andres/repos/rat-mapping-paper/configs/J_SHAP/config.toml"
+DATA_PATH = "/home/andres/data/hbmep-processed/J_SHAP/data.csv"
+
+INFERENCE_FILE = "inference.pkl"
+
+BUILD_DIR = "/home/andres/repos/rat-mapping-paper/reports/J_SHAP"
