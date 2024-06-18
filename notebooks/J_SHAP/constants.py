357e4d5b4ccbf7a578de27bf544a94a3f377717b
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-06-18 12:32:37 -0400

    update paths

--- notebooks/J_SHAP/constants.py
+++ notebooks/J_SHAP/constants.py
@@ -1,6 +1,8 @@
-TOML_PATH = "/home/andres/repos/rat-mapping-paper/configs/J_SHAP/config.toml"
-DATA_PATH = "/home/andres/data/hbmep-processed/J_SHAP/data.csv"
+USER = "andres"
+
+TOML_PATH = f"/home/{USER}/repos/rat-mapping-paper/configs/J_SHAP/config.toml"
+DATA_PATH = f"/home/{USER}/data/hbmep-processed/J_SHAP/data.csv"
 
 INFERENCE_FILE = "inference.pkl"
 
-BUILD_DIR = "/home/andres/repos/rat-mapping-paper/reports/J_SHAP"
+BUILD_DIR = f"/home/{USER}/repos/rat-mapping-paper/reports/J_SHAP"
