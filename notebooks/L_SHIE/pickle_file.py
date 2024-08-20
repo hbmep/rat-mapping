bb52da4d82709048a86df9c2f5a1517c3577b5eb
apleone <apleone97@gmail.com>
2024-08-20 14:06:28 -0400

    Begin selectivity analysis

---
+++ notebooks/L_SHIE/pickle_file.py
@@ -0,0 +1,30 @@
+import os
+
+import pickle
+import pandas as pd
+
+from hbmep.model.utils import Site as site
+
+from constants import(
+    DATA_PATH
+)
+
+if __name__ == "__main__":
+    src = DATA_PATH 
+    d = {}
+    df = pd.read_csv(src)
+    combo = df[["participant", "compound_position", "compound_charge_params"]].apply(tuple, axis=1).unique().tolist()
+    muscle = ["LADM", "LBiceps", "LDeltoid", "LECR", "LFCR", "LTriceps"]
+    combo = [(c[0], c[1], c[2], m) for c in combo for m in muscle]
+
+    for par, pos, charge, musc in combo:
+        print(par, pos, musc)
+        src= f"/home/andres/repos/rat-mapping-paper/reports/L_SHIE/sub__{par}/pos__{pos}/charge__   {charge}/resp__{musc}/non_hierarchical_bayesian_model/inference.pkl"
+        with open(src, "rb") as f:
+            _,_,posterior_samples = pickle.load(f)
+
+        d[(par, pos, charge, musc)] = posterior_samples 
+    
+    path = "/home/andres/repos/rat-mapping-paper/reports/L_SHIE/combine.pkl"
+    with open(path, 'wb') as handle:
+        pickle.dump(d, handle, protocol=pickle.HIGHEST_PROTOCOL)
