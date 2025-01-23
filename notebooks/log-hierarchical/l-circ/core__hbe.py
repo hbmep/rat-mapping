d784fbf316e5a00c2ad9a31b28cc8c7bd67636a0
vishu-tyagi <vishweshwartyagiv@gmail.com>
2025-01-23 14:10:17 -0500

    add estimation model

---
+++ notebooks/log-hierarchical/l-circ/core__hbe.py
@@ -0,0 +1,118 @@
+import os
+import sys
+import pickle
+import logging
+
+import pandas as pd
+import numpy as np
+from hbmep.config import Config
+from hbmep.utils import timing
+
+from paper.utils import setup_logging
+from models import (
+    HBe,
+)
+from constants import (
+    TOML_PATH,
+    DATA_PATH,
+    BUILD_DIR,
+    INFERENCE_FILE
+)
+
+logger = logging.getLogger(__name__)
+
+
+@timing
+def main(M):
+    src = DATA_PATH
+    data = pd.read_csv(src)
+
+    config = Config(toml_path=TOML_PATH)
+    config.BASE = 1
+    config.RESPONSE = [config.RESPONSE[1], config.RESPONSE[2], config.RESPONSE[4]]
+
+    model = M(config=config)
+    model.build_dir = os.path.join(
+        BUILD_DIR,
+        model.NAME,
+        "diam"
+    )
+
+    # Logging
+    os.makedirs(model.build_dir, exist_ok=True)
+    setup_logging(
+        dir=model.build_dir,
+        fname=os.path.basename(__file__)
+    )
+
+    ind = data[model.intensity] > 0
+    df = data[ind].reset_index(drop=True).copy()
+
+    vertices = [
+        '-C6LE', '-C6LN', '-C6LNE', '-C6LNW',
+        '-C6LS', '-C6LSE', '-C6LSW', '-C6LW'
+    ]
+    diam = [
+        'C6LE-C6LW', 'C6LNE-C6LSW', 'C6LS-C6LN', 'C6LSE-C6LNW'
+    ]
+    radii = [
+        'C6LE-C6LC', 'C6LN-C6LC', 'C6LNE-C6LC', 'C6LNW-C6LC',
+        'C6LS-C6LC', 'C6LSE-C6LC', 'C6LSW-C6LC', 'C6LW-C6LC',
+    ]
+    subset = ['-C6LC'] + diam
+    ind = df[model.features[1]].isin(subset)
+    df = df[ind].reset_index(drop=True).copy()
+
+    subset = ['amap01', 'amap02']
+    ind = df[model.features[0]].isin(subset)
+    df = df[ind].reset_index(drop=True).copy()
+
+    # Run inference
+    df, encoder_dict = model.load(df=df)
+    df[model.intensity] = np.log(df[model.intensity])
+    logger.info(f"df.shape {df.shape}")
+    logger.info(
+        f"Running {M.NAME} with response {config.RESPONSE} ..."
+    )
+    _, posterior_samples = model.run(df=df, **model.run_kwargs)
+
+    # Predictions and recruitment curves
+    prediction_df = model.make_prediction_dataset(df=df)
+    posterior_predictive = model.predict(
+        df=prediction_df, posterior_samples=posterior_samples
+    )
+    model.render_recruitment_curves(
+        df=df,
+        encoder_dict=encoder_dict,
+        posterior_samples=posterior_samples,
+        prediction_df=prediction_df,
+        posterior_predictive=posterior_predictive
+    )
+    model.render_predictive_check(
+        df=df,
+        encoder_dict=encoder_dict,
+        prediction_df=prediction_df,
+        posterior_predictive=posterior_predictive
+    )
+
+    summary_df = model.summary(posterior_samples)
+    logger.info(f"Summary:\n{summary_df.to_string()}")
+    dest = os.path.join(model.build_dir, "summary.csv")
+    summary_df.to_csv(dest)
+    logger.info(f"Saved summary to {dest}")
+
+    # Save
+    src = os.path.join(model.build_dir, INFERENCE_FILE)
+    with open(src, "wb") as f:
+        pickle.dump((df, encoder_dict, model, posterior_samples,), f)
+
+    logger.info(
+        f"Finished running {model.NAME} with response {config.RESPONSE}"
+    )
+    logger.info(f"Saved results to {model.build_dir}")
+    return
+
+
+if __name__ == "__main__":
+    M = HBe
+    main(M=M)
--- notebooks/log-hierarchical/l-circ/models.py
+++ notebooks/log-hierarchical/l-circ/models.py
@@ -136,3 +136,165 @@ class HierarchicalBayesianModel(GammaModel):
                     Mixture,
                     obs=response_obs
                 )
+
+
+class HBe(GammaModel):
+    NAME = "estimation"
+
+    def __init__(self, config: Config):
+        super(HBe, self).__init__(config=config)
+        self.mcmc_params = {
+            # "num_warmup": 4000,
+            # "num_samples": 4000,
+            "num_warmup": 200,
+            "num_samples": 200,
+            "num_chains": 4,
+            # "thinning": 4,
+            "thinning": 1,
+        }
+        self.run_kwargs = {
+            "max_tree_depth": (20, 20),
+            "target_accept_prob": .95,
+            "extra_fields": [
+                "potential_energy",
+                "num_steps",
+                "accept_prob",
+            ]
+        }
+        self.NAME += (
+            f'_{self.mcmc_params["num_warmup"]}W'
+            f'_{self.mcmc_params["num_samples"]}S'
+            f'_{self.mcmc_params["num_chains"]}C'
+            f'_{self.mcmc_params["thinning"]}T'
+            f'_{self.run_kwargs["max_tree_depth"][0]}D'
+            f'_{self.run_kwargs["target_accept_prob"]}A'
+        )
+
+    def _model(self, intensity, features, response_obs=None):
+        n_data = intensity.shape[0]
+        n_features = np.max(features, axis=0) + 1
+        feature0 = features[..., 0]
+        feature1 = features[..., 1]
+
+        n_fixed = 1
+        n_delta = n_features[1] - 1
+
+        # # Fixed
+        # a_fixed_loc = numpyro.sample("a_fixed_loc", dist.Normal(3., 5.))
+        # a_fixed_scale = numpyro.sample("a_fixed_scale", dist.HalfNormal(5.))
+
+        with numpyro.plate(site.n_response, self.n_response):
+            with numpyro.plate("n_fixed", n_fixed):
+                with numpyro.plate(site.n_features[0], n_features[0]):
+                    a_fixed = numpyro.sample("a_fixed", dist.Normal(3., 5.))
+                    # a_fixed_raw = numpyro.sample(
+                    #     "a_fixed_raw", dist.Normal(0., 1.)
+                    # )
+                    # a_fixed = numpyro.deterministic(
+                    #     "a_fixed", a_fixed_loc + (a_fixed_scale * a_fixed_raw)
+                    # )
+
+        # Delta
+        with numpyro.plate("n_delta", n_delta):
+            a_delta_loc = numpyro.sample("a_delta_loc", dist.Normal(0., 5.))
+            a_delta_scale = numpyro.sample("a_delta_scale", dist.HalfNormal(5.))
+
+            with numpyro.plate(site.n_response, self.n_response):
+                with numpyro.plate(site.n_features[0], n_features[0]):
+                    a_delta_raw = numpyro.sample("a_delta_raw", dist.Normal(0., 1.))
+                    a_delta = numpyro.deterministic(
+                        "a_delta", a_delta_loc + (a_delta_scale * a_delta_raw)
+                    )
+
+        with numpyro.plate(site.n_response, self.n_response):
+            with numpyro.plate("n_delta", n_delta):
+                with numpyro.plate(site.n_features[0], n_features[0]):
+                    a_fixed_plus_delta = a_fixed + jnp.swapaxes(a_delta, -1, -2)
+
+        # Hyper-priors
+        b_scale = numpyro.sample("b_scale", dist.HalfNormal(5.))
+        L_scale = numpyro.sample("L_scale", dist.HalfNormal(.1))
+        ell_scale = numpyro.sample("ell_scale", dist.HalfNormal(1.))
+        H_scale = numpyro.sample("H_scale", dist.HalfNormal(5.))
+
+        c_1_scale = numpyro.sample("c_1_scale", dist.HalfNormal(5.))
+        c_2_scale = numpyro.sample("c_2_scale", dist.HalfNormal(.5))
+
+        with numpyro.plate(site.n_response, self.n_response):
+            with numpyro.plate(site.n_features[1], n_features[1]):
+                with numpyro.plate(site.n_features[0], n_features[0]):
+                    # Priors
+                    a = numpyro.deterministic(
+                        site.a,
+                        jnp.concatenate([a_fixed, a_fixed_plus_delta], axis=1)
+                    )
+
+                    b_raw = numpyro.sample("b_raw", dist.HalfNormal(scale=1))
+                    b = numpyro.deterministic(site.b, jnp.multiply(b_scale, b_raw))
+
+                    L_raw = numpyro.sample("L_raw", dist.HalfNormal(scale=1))
+                    L = numpyro.deterministic(site.L, jnp.multiply(L_scale, L_raw))
+
+                    ell_raw = numpyro.sample("ell_raw", dist.HalfNormal(scale=1))
+                    ell = numpyro.deterministic(site.ell, jnp.multiply(ell_scale, ell_raw))
+
+                    H_raw = numpyro.sample("H_raw", dist.HalfNormal(scale=1))
+                    H = numpyro.deterministic(site.H, jnp.multiply(H_scale, H_raw))
+
+                    c_1_raw = numpyro.sample("c_1_raw", dist.HalfNormal(scale=1))
+                    c_1 = numpyro.deterministic(site.c_1, jnp.multiply(c_1_scale, c_1_raw))
+
+                    c_2_raw = numpyro.sample("c_2_raw", dist.HalfNormal(scale=1))
+                    c_2 = numpyro.deterministic(site.c_2, jnp.multiply(c_2_scale, c_2_raw))
+
+        # # Outlier Distribution
+        # q = numpyro.sample(site.outlier_prob, dist.Uniform(0., 0.01))
+
+        with numpyro.plate(site.n_response, self.n_response):
+            with numpyro.plate(site.n_data, n_data):
+                # Model
+                mu = numpyro.deterministic(
+                    site.mu,
+                    S.rectified_logistic(
+                        x=intensity,
+                        a=a[feature0, feature1],
+                        b=b[feature0, feature1],
+                        L=L[feature0, feature1],
+                        ell=ell[feature0, feature1],
+                        H=H[feature0, feature1],
+                        eps=EPS
+                    )
+                )
+                beta = numpyro.deterministic(
+                    site.beta,
+                    self.rate(
+                        mu,
+                        c_1[feature0, feature1],
+                        c_2[feature0, feature1]
+                    )
+                )
+                alpha = numpyro.deterministic(
+                    site.alpha,
+                    self.concentration(mu, beta)
+                )
+
+                # # Mixture
+                # mixing_distribution = dist.Categorical(
+                #     probs=jnp.stack([1 - q, q], axis=-1)
+                # )
+                # component_distributions=[
+                #     dist.Gamma(concentration=alpha, rate=beta),
+                #     dist.HalfNormal(scale=L[feature0, feature1] + H[feature0, feature1])
+                # ]
+                # Mixture = dist.MixtureGeneral(
+                #     mixing_distribution=mixing_distribution,
+                #     component_distributions=component_distributions
+                # )
+
+                # Observation
+                numpyro.sample(
+                    site.obs,
+                    dist.Gamma(concentration=alpha, rate=beta),
+                    # Mixture,
+                    obs=response_obs
+                )
