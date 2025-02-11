0e8d5e8831a08a62c72c2727741ecf643e577150
vishu-tyagi <vishweshwartyagiv@gmail.com>
2025-02-11 10:22:19 -0500

    fix typo in model

--- notebooks/log-hierarchical/l-circ/core__hbe.py
+++ notebooks/log-hierarchical/l-circ/core__hbe.py
@@ -9,43 +9,32 @@ from hbmep.config import Config
 from hbmep.utils import timing
 
 from paper.utils import setup_logging
-from models import (
-    HBe,
-)
+from models import HBe
 from constants import (
     TOML_PATH,
     DATA_PATH,
     BUILD_DIR,
-    INFERENCE_FILE
+    INFERENCE_FILE,
+    MODEL_FILE,
+    MAP,
+    VERTICES,
+    DIAM,
+    RADII
 )
 
 logger = logging.getLogger(__name__)
-VERTICES = [
-    '-C6LE', '-C6LN', '-C6LNE', '-C6LNW',
-    '-C6LS', '-C6LSE', '-C6LSW', '-C6LW'
-]
-DIAM = [
-    'C6LE-C6LW', 'C6LNE-C6LSW', 'C6LS-C6LN', 'C6LSE-C6LNW'
-]
-RADII = [
-    'C6LE-C6LC', 'C6LN-C6LC', 'C6LNE-C6LC', 'C6LNW-C6LC',
-    'C6LS-C6LC', 'C6LSE-C6LC', 'C6LSW-C6LC', 'C6LW-C6LC',
-]
 
 
 @timing
-def main(M, run_id):
+def main(model, run_id):
     assert run_id in {"diam", "radii", "vertices", "all"}
     src = DATA_PATH
     data = pd.read_csv(src)
-
-    config = Config(toml_path=TOML_PATH)
-    config.BASE = 1
-    model = M(config=config)
     model.build_dir = os.path.join(
         BUILD_DIR,
         model.NAME,
-        run_id
+        model.subname,
+        run_id,
     )
 
     # Logging
@@ -57,27 +46,30 @@ def main(M, run_id):
 
     ind = data[model.intensity] > 0
     df = data[ind].reset_index(drop=True).copy()
+    df[model.features[1]] = df[model.features[1]].replace(MAP)
 
     # subset = ['amap01', 'amap02']
     # ind = df[model.features[0]].isin(subset)
     # df = df[ind].reset_index(drop=True).copy()
 
-    subset = ['-C6LC']
-    if run_id == "diam": subset += DIAM
-    if run_id == "radii": subset += RADII
-    if run_id == "vertices": subset += VERTICES
-    if run_id == "all": subset += DIAM + RADII + VERTICES
+    subset = []
+    if run_id == "diam": subset += DIAM; reference = "-C"; subset += [reference]
+    if run_id == "radii": subset += RADII; reference = "-C"; subset += [reference]
+    if run_id == "vertices": subset += VERTICES; reference = "S-N"; subset += [reference]
+    if run_id == "all": subset += DIAM + RADII + VERTICES; reference = "-C";
+    assert set(subset) <= set(df[model.features[1]].values.tolist())
     ind = df[model.features[1]].isin(subset)
     df = df[ind].reset_index(drop=True).copy()
+    df[model.features[1]] = df[model.features[1]].replace({reference: " " + reference})
 
     # Run inference
     df, encoder_dict = model.load(df=df)
     df[model.intensity] = np.log(df[model.intensity])
     logger.info(f"df.shape {df.shape}")
     logger.info(
-        f"Running {model.NAME} with {run_id} - {subset} ..."
+        f"Running {model.NAME} with {run_id} - {subset} and reference {reference} ..."
     )
-    _, posterior_samples = model.run(df=df, **model.run_kwargs)
+    mcmc, posterior_samples = model.run(df=df, **model.run_kwargs)
 
     # Predictions and recruitment curves
     prediction_df = model.make_prediction_dataset(df=df)
@@ -91,12 +83,12 @@ def main(M, run_id):
         prediction_df=prediction_df,
         posterior_predictive=posterior_predictive
     )
-    model.render_predictive_check(
-        df=df,
-        encoder_dict=encoder_dict,
-        prediction_df=prediction_df,
-        posterior_predictive=posterior_predictive
-    )
+    # model.render_predictive_check(
+    #     df=df,
+    #     encoder_dict=encoder_dict,
+    #     prediction_df=prediction_df,
+    #     posterior_predictive=posterior_predictive
+    # )
 
     summary_df = model.summary(posterior_samples)
     logger.info(f"Summary:\n{summary_df.to_string()}")
@@ -107,16 +99,26 @@ def main(M, run_id):
     # Save
     src = os.path.join(model.build_dir, INFERENCE_FILE)
     with open(src, "wb") as f:
-        pickle.dump((df, encoder_dict, model, posterior_samples,), f)
+        pickle.dump((df, encoder_dict, mcmc, posterior_samples,), f)
 
-    logger.info(
-        f"Finished running {model.NAME} with {run_id} - {subset}"
-    )
+    src = os.path.join(model.build_dir, MODEL_FILE)
+    with open(src, "wb") as f:
+        pickle.dump((model,), f)
+
+    logger.info(f"Finished running {model.NAME} with {run_id} - {subset}")
     logger.info(f"Saved results to {model.build_dir}")
     return
 
 
 if __name__ == "__main__":
-    run_id = sys.argv[1:][0]
+    run_id = sys.argv[1]
+    config = Config(toml_path=TOML_PATH)
+    config.BASE = 1
+
     M = HBe
-    main(M=M, run_id=run_id)
+    model = M(config=config)
+    model._model = model.mvn_reference
+    # model._model = model.mvn_hb_reference
+    model.NAME = model._model.__name__
+    main(model=model, run_id=run_id)
+
--- notebooks/log-hierarchical/l-circ/models.py
+++ notebooks/log-hierarchical/l-circ/models.py
@@ -163,6 +163,9 @@ class HBe(GammaModel):
             "num_warmup": 4000,
             "num_samples": 4000,
             "thinning": 4,
+            # "num_warmup": 1000,
+            # "num_samples": 1000,
+            # "thinning": 1,
             # "num_warmup": 400,
             # "num_samples": 400,
             # "thinning": 1,
@@ -334,7 +337,7 @@ class HBe(GammaModel):
         with numpyro.plate("n_delta", n_delta):
             a_delta_loc = numpyro.sample("a_delta_loc", dist.Normal(0., 5.))
             a_delta_scale = numpyro.sample("a_delta_scale", dist.HalfNormal(5.))
-            Rho = numpyro.sample("Rho", dist.LKJ(self.n_response, self.n_response))
+            Rho = numpyro.sample("Rho", dist.LKJ(self.n_response, 1.))
 
             with numpyro.plate(site.n_features[0], n_features[0]):
                 a_delta_raw = numpyro.sample(
@@ -463,7 +466,7 @@ class HBe(GammaModel):
             a_delta_scale_m = numpyro.sample("a_delta_scale_m", dist.HalfNormal(5.))
 
             a_delta_scale = numpyro.sample("a_delta_scale", dist.HalfNormal(5.))
-            Rho = numpyro.sample("Rho", dist.LKJ(self.n_response, self.n_response))
+            Rho = numpyro.sample("Rho", dist.LKJ(self.n_response, 1.))
 
             with numpyro.plate(self.n_response, self.n_response):
                 a_delta_loc_m_raw = numpyro.sample("a_delta_loc_m_raw", dist.Normal(0., 1.))
@@ -575,3 +578,112 @@ class HBe(GammaModel):
                     obs=response_obs
                 )
 
+    def mvn_no_reference(self, intensity, features, response_obs=None):
+        n_data = intensity.shape[0]
+        n_features = np.max(features, axis=0) + 1
+        feature0 = features[..., 0]
+        feature1 = features[..., 1]
+
+        # Fixed and delta
+        with numpyro.plate(site.n_features[1], n_features[1]):
+            a_fixed_loc = numpyro.sample("a_fixed_loc", dist.Normal(3., 5.))
+            a_delta_loc = numpyro.sample("a_delta_loc", dist.Normal(0., 5.))
+
+            a_scale = numpyro.sample("a_scale", dist.HalfNormal(5.))
+            Rho = numpyro.sample("Rho", dist.LKJ(self.n_response, 1.))
+
+            a_loc = numpyro.deterministic("a_loc", a_fixed_loc[:, None] + a_delta_loc[:, None])
+
+            with numpyro.plate(site.n_features[0], n_features[0]):
+                a_raw = numpyro.sample(
+                    "a_raw",
+                    dist.MultivariateNormal(0, (a_scale[:, None, None] @ a_scale[:, None, None]) * Rho)
+                )
+                a = numpyro.deterministic(site.a, a_loc + a_raw)
+
+        # Hyper-priors
+        b_scale = numpyro.sample("b_scale", dist.HalfNormal(10.))
+        L_scale = numpyro.sample("L_scale", dist.HalfNormal(.1))
+        ell_scale = numpyro.sample("ell_scale", dist.HalfNormal(1.))
+        H_scale = numpyro.sample("H_scale", dist.HalfNormal(5.))
+
+        c_1_scale = numpyro.sample("c_1_scale", dist.HalfNormal(5.))
+        c_2_scale = numpyro.sample("c_2_scale", dist.HalfNormal(.5))
+
+        with numpyro.plate(site.n_response, self.n_response):
+            with numpyro.plate(site.n_features[1], n_features[1]):
+                with numpyro.plate(site.n_features[0], n_features[0]):
+                    # Priors
+                    b_raw = numpyro.sample("b_raw", dist.HalfNormal(scale=1))
+                    b = numpyro.deterministic(site.b, jnp.multiply(b_scale, b_raw))
+
+                    L_raw = numpyro.sample("L_raw", dist.HalfNormal(scale=1))
+                    L = numpyro.deterministic(site.L, jnp.multiply(L_scale, L_raw))
+
+                    ell_raw = numpyro.sample("ell_raw", dist.HalfNormal(scale=1))
+                    ell = numpyro.deterministic(site.ell, jnp.multiply(ell_scale, ell_raw))
+
+                    H_raw = numpyro.sample("H_raw", dist.HalfNormal(scale=1))
+                    H = numpyro.deterministic(site.H, jnp.multiply(H_scale, H_raw))
+
+                    c_1_raw = numpyro.sample("c_1_raw", dist.HalfNormal(scale=1))
+                    c_1 = numpyro.deterministic(site.c_1, jnp.multiply(c_1_scale, c_1_raw))
+
+                    c_2_raw = numpyro.sample("c_2_raw", dist.HalfNormal(scale=1))
+                    c_2 = numpyro.deterministic(site.c_2, jnp.multiply(c_2_scale, c_2_raw))
+
+        if self.use_mixture:
+            # Outlier Distribuion
+            q = numpyro.sample(site.outlier_prob, dist.Uniform(0., 0.01))
+
+        with numpyro.plate(site.n_response, self.n_response):
+            with numpyro.plate(site.n_data, n_data):
+                # Model
+                mu = numpyro.deterministic(
+                    site.mu,
+                    SF.rectified_logistic(
+                        x=intensity,
+                        a=a[feature0, feature1],
+                        b=b[feature0, feature1],
+                        L=L[feature0, feature1],
+                        ell=ell[feature0, feature1],
+                        H=H[feature0, feature1],
+                        eps=EPS
+                    )
+                )
+                beta = numpyro.deterministic(
+                    site.beta,
+                    self.rate(
+                        mu,
+                        c_1[feature0, feature1],
+                        c_2[feature0, feature1]
+                    )
+                )
+                alpha = numpyro.deterministic(
+                    site.alpha,
+                    self.concentration(mu, beta)
+                )
+
+                if self.use_mixture:
+                    # Mixture
+                    mixing_distribution = dist.Categorical(
+                        probs=jnp.stack([1 - q, q], axis=-1)
+                    )
+                    component_distributions=[
+                        dist.Gamma(concentration=alpha, rate=beta),
+                        dist.HalfNormal(scale=L[feature0, feature1] + H[feature0, feature1])
+                    ]
+                    Mixture = dist.MixtureGeneral(
+                        mixing_distribution=mixing_distribution,
+                        component_distributions=component_distributions
+                    )
+
+                # Observation
+                numpyro.sample(
+                    site.obs,
+                    (
+                        Mixture if self.use_mixture
+                        else dist.Gamma(concentration=alpha, rate=beta)
+                    ),
+                    obs=response_obs
+                )
