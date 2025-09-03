a35768a5e43d4adf1efe84dab6ecf5c4ea5756b4
vishu-tyagi <vishweshwartyagiv@gmail.com>
2025-09-03 10:26:26 -0400

    clean up

--- notebooks/C_SMA_LAR/constants.py
+++
@@ -1,6 +0,0 @@
-TOML_PATH = "/home/andres/repos/rat-mapping-paper/configs/C_SMA_LAR/config.toml"
-DATA_PATH = "/home/andres/data/hbmep-processed/C_SMA_LAR/data.csv"
-
-INFERENCE_FILE = "inference.pkl"
-
-BUILD_DIR = "/home/andres/repos/rat-mapping-paper/reports/C_SMA_LAR"
--- notebooks/basic-setup/constants.py
+++
@@ -1,8 +0,0 @@
-import os
-
-TOML_PATH = "/Users/andres/repos/rat-mapping-paper/configs/basic-setup/config.toml"
-DATA_PATH = "/Users/andres/repos/rat-mapping-paper/notebooks/basic-setup/sample_data.csv"
-BUILD_DIR = "/Users/andres/repos/rat-mapping-paper/reports/basic-setup/"
-
-INFERENCE_FILE = "inference.pkl"
-NETCODE_FILE = "inference_data.nc"
