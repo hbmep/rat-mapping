0a0c2677b7d9819cb7346e91ef7b50299c2201e4
vishu-tyagi <vishweshwartyagiv@gmail.com>
2025-09-04 15:40:29 -0400

    add circ estimation results

---
+++ notebooks/circ/constants.py
@@ -0,0 +1,7 @@
+from paper.util import get_paths
+from paper.constants.circ import (
+    EXPERIMENT,
+    MAP, VERTICES, DIAM, RADII
+)
+
+BUILD_DIR, TOML_PATH, DATA_PATH, MEP_MATRIX_PATH = get_paths(EXPERIMENT)
