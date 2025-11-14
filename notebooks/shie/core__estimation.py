24cf7121cdfff9cbabebcdefd5fe08e4280dca99
vishu-tyagi <vishweshwartyagiv@gmail.com>
2025-11-14 14:06:19 -0500

    add estimation model for circ

--- notebooks/shie/core__estimation.py
+++ notebooks/shie/core__estimation.py
@@ -12,6 +12,9 @@ from constants import BUILD_DIR, TOML_PATH
 
 logger = logging.getLogger(__name__)
 
+TEST_RUN = False
+# TEST_RUN = True
+
 
 @timing
 def main(model):
@@ -46,9 +49,12 @@ def main(model):
 
 if __name__ == "__main__":
     model = Estimation(toml_path=TOML_PATH)
-    model.test_run = True
+    model.test_run = TEST_RUN
 
-    model._model = model.circ_est_mvn_reference_rl_masked
+    # model._model = model.circ_est_mvn_reference_rl_masked
+    # model._model = model.robust_circ_est_mvn_reference_rl_masked
+    model._model = model.circ_est_mvn_reference_rl_masked_altfix
+    # model._model = model.robust_circ_est_mvn_reference_rl_masked_altfix
     model.use_mixture = True
     model.run_id = "all"
 
